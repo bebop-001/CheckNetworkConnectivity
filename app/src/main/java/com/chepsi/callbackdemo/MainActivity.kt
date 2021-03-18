93df1eecd6ec1ee73ea8b74fe1e44906b3a8ff91
sjs <sjs@sonic.net>
2021-03-18 09:09:25 -0700

    added Readme.  Changed the "hello" textview to show network connection status.

--- app/src/main/java/com/chepsi/callbackdemo/MainActivity.kt
+++ app/src/main/java/com/chepsi/callbackdemo/MainActivity.kt
@@ -2,16 +2,19 @@ package com.chepsi.callbackdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var helloTV: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        helloTV = findViewById(R.id.hello_TV)
         if (Variables.isNetworkConnected){
-            //Do something when network is connected
+            helloTV.setText("Hello.  Network is connected")
         }
         else {
-            //Do something else when network is not connected
+            helloTV.setText("Hello.  Failed to get network connection.")
         }
     }
 }
