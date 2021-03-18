3b5d67217a355170af4f856d04565b6589d4b315
sjs <sjs@sonic.net>
2021-03-18 11:47:50 -0700

    Renamed object

--- app/src/main/java/com/chepsi/callbackdemo/GlobalVariables.kt
+++ app/src/main/java/com/chepsi/callbackdemo/GlobalVariables.kt
@@ -3,7 +3,7 @@ package com.chepsi.callbackdemo
 import android.util.Log
 import kotlin.properties.Delegates
 
-object Variables {
+object GlobalVariables {
     var isNetworkConnected: Boolean by Delegates.observable(false) { property, oldValue, newValue ->
         Log.i("Network connectivity", "$newValue")
     }
